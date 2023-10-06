b213ac31025e8e5aa10854e4d2638f2bc7efe8b9
geofannyher <geofannyher@gmail.com>
2023-10-06 09:44:43 +0700

    fix: chat admin

--- src/pages/admin/chatService/index.tsx
+++ src/pages/admin/chatService/index.tsx
@@ -41,7 +41,7 @@ const ChatService = () => {
   const [chat, setChat] = useState<string>("");
   const [showAttachOptions, setShowAttachOptions] = useState(false);
   const socket: Socket = io("https://api.flattenbot.site");
-  const idUser = Cookie.get("id");
+  const idUser = Cookie.get("uid");
   const token = Cookie.get("token");
   const navigate = useNavigate()
   const role = Cookie.get('role')
@@ -51,7 +51,6 @@ const ChatService = () => {
     setShowAttachOptions(!showAttachOptions);
   };
 
-  console.log(id)
   useEffect(() => {
     socket.on("adminMenerima", (message: any) => {
       setMessages((prevMessages) => {
@@ -106,8 +105,6 @@ const ChatService = () => {
   };
 
   const sendMessage = async () => {
-    console.log(chat, 'isi chat');
-    console.log(id, 'ini id room');
     socket.emit("adminMessage", {
       content: chat,
       idroom: id,
@@ -181,7 +178,6 @@ const ChatService = () => {
               <div className="pt-14">
                 {/* Ganti nilaix nnt menjadi index */}
                 {room.map((element, index) => {
-                  console.log(element);
                   return (
                     <div key={index}>
                       <div
